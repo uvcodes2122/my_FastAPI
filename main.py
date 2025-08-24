06f5ffc2c76eb326a8e7eab3bc0862519287a909
uvcodes2122 <uvcodes2122@gmail.com>
2025-08-24 18:50:08 +0530

    revert the last change

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def hello():
 def about():
     return {"message": "This is about page of my fastapi project."}
 
-@app.get("/test1")
+@app.get("/test")
 def test():
     return {"message": "This is test page of my fastapi project."}
 
