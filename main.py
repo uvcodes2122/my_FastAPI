8ed1b4dd9b7e734274c8c298bf901053f5753a8d
uvcodes2122 <uvcodes2122@gmail.com>
2025-08-24 18:38:54 +0530

    changed method from test to test1

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def hello():
 def about():
     return {"message": "This is about page of my fastapi project."}
 
-@app.get("/test")
+@app.get("/test1")
 def test():
     return {"message": "This is test page of my fastapi project."}
 
